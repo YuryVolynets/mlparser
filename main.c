510d9e8d39a1c7dff27cca5f7b0b671a75f7ca86
YuryVolynets <yury.volynets@gmail.com>
2016-10-14 18:07:17 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("usage: %s <email> <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(*(argv + 2), "r");
+    if (!fp)
+    {
+        perror("File opening failed");
+        return EXIT_FAILURE;
+    }
+
+    while (getline(&line, &len, fp) != -1)
+    {
+        if (strstr(line, *(argv + 1)))
+        {
+            printf("%s", line);
+        }
+    }
+
+    if (ferror(fp))
+    {
+        puts("I/O error when reading");
+    } else if (feof(fp))
+    {
+        puts("End of file reached successfully");
+    }
+
+    free(line);
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
